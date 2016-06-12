c4a4c5e6277ff76403595b3179448f8978ed13af
3eeedeeess <dereksayles@gmail.com>
2016-06-11 20:51:36 -0700

    Example of Python dictionaries and lists

---
+++ python_adventurer.py
@@ -0,0 +1,9 @@
+inventory = {
+			'gold' : 500,
+			'burlap_sack' : ['dagger', 'binoculars', 'map'],
+			'medical_pouch' : ['painkillers', 'bandages', 'antibiotics']
+}
+
+inventory['protective_case'] = 'dangerous artifact'
+
+print inventory
